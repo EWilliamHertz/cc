27d148f8dbc3da0145893e9b6ede292999502446
ewilliamhertz <ewilliamhe@gmail.com>
2026-06-15 14:47:05 +0000

    fixed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['3000-cs-553118797525-default.cs-europe-west4-pear.cloudshell.dev'],
-  serverexternalpackages: ["@prisma/client", "prisma", "pg"],
+  serverExternalPackages: ["@prisma/client", "prisma", "pg"],
   
   images: {
     remotePatterns: [
